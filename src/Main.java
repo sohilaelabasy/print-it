a2342aa461b930ca32c1168c93f76ec438976791
sohila elabasy <sohailaismail6@gmail.com>
2025-03-03 16:50:27 +0200

    adding said

--- src/Main.java
+++ src/Main.java
@@ -2,10 +2,11 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+        //SAID
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
         System.out.printf("welcome!");
         System.out.println("hello from said" );
-
+        System.out.println("welcome from said ");
     }
 }

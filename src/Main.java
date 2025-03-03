5dfd4162f9318baaa6c8993557c04b6a8f6642d2
sohila elabasy <sohailaismail6@gmail.com>
2025-03-03 17:06:39 +0200

    adding feature/A

--- src/Main.java
+++ src/Main.java
@@ -4,9 +4,13 @@ public class Main {
     public static void main(String[] args) {
       //ali
 // TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+        //SAID
+        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
         System.out.printf("welcome!");
         System.out.println("hello from said" );
+        System.out.println("welcome from said ");
         System.out.println("hello from ali");
+        System.out.println("feature/A");
     }
 }

a3a44eec8511a4ec531e83d12d6810a44619e0ce
sohila elabasy <sohailaismail6@gmail.com>
2025-03-03 16:49:04 +0200

    adding welcome ali

--- src/Main.java
+++ src/Main.java
@@ -2,10 +2,11 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+      //ali
+// TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
         System.out.printf("welcome!");
         System.out.println("hello from said" );
-
+        System.out.println("hello from ali");
     }
 }

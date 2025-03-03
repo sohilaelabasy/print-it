86f369711340518eb3379e3b0aefc6d173b4a1bd
sohila elabasy <sohailaismail6@gmail.com>
2025-03-03 16:27:34 +0200

    adding welcome msg from said

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,7 @@ public class Main {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
         System.out.printf("welcome!");
-
+        System.out.println("hello from said" );
 
     }
 }
